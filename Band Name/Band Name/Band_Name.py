a9a396a9cd7e5193be6ffa801ff9fd150a5a6308
mrfatihgul <fatihgulrose@outlook.com>
2024-06-30 20:02:51 +0300

    Band Name
    An educational project for me.

---
+++ Band Name/Band Name/Band_Name.py
@@ -0,0 +1,7 @@
+#Band Name Processor:
+
+city = str(input("Enter the city where you born: "))
+
+pet_name = str(input("Enter your pets name: "))
+
+print(f'Your bands name should be "{pet_name}'s Hell in {city} City"')
